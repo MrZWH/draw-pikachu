12ee65c9dad645400432d91cfc396e1079796170
牛奶小哥哥 <1875290692@qq.com>
2018-02-24 20:13:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 !function() {
+  let duration = 20
   function writeCode (prefix, code, fn) {
     let container = document.querySelector('#code')
     let styleTag = document.querySelector('#styleTag')
     let n = 0
-    let id = setInterval(() => {
+    setTimeout(function run () {
       n+=1
       container.innerHTML = code.substring(0, n)
       styleTag.innerHTML = code.substring(0, n)
       container.scrollTop = container.scrollHeight
-      if (n >= code.length) {
-        window.clearInterval(id)
+      if (n < code.length) {
+        setTimeout(run, duration)
+      } else {
         fn && fn.call()
       }
-    }, 20)
+    }, duration)
   }
   
   let code = `
@@ -207,4 +209,21 @@
 */
 `
   writeCode('', code)
+  $('.actions').on('click', 'button', function () {
+      let $button = $(e.currentTarget)
+      let speed = $button.attr('data-speed')
+      $button.addClass('active')
+        .siblings('.active').removeClass('active')
+      switch(speed) {
+        case 'slow':
+          duration = 100
+          break
+        case 'normal':
+          duration = 20
+          break
+        case 'fast':
+          duration = 10
+          break
+      }
+  })
 }.call()
